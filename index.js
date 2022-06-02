4853980fe75ca97fde3acf3e8c2d3eb3e445f1c4
versalitas <100954079+versalitas@users.noreply.github.com>
2022-06-02 17:48:51 +0200

    Add files via upload
    fixing bugs

--- index.js
+++ index.js
@@ -3,31 +3,27 @@
 
 const express = require('express');
 
+//creates an instance of express to serve the end points
+const app = express();
+
 const fs = require('fs');
 
 const currentUrl = 'http://localhost';
 
 //PORT defining as environmental variable
 // in terminal $export PORT=3001 or another value 
-const port = process.env.PORT || 3000;
-//creates an instance of express to serve the end points
-const app = express();
+const port = process.env.PORT || 3001;
+
 
 //handling routes
 const routes = require('./routes/routes.js')(app, fs);
-
 
 //add middleware for parsing json
 app.use(express.json());
 
 
 
-
-
-
-
-
-  // starting the server
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+// starting the server
+const server = app.listen(port, () => {
+    console.log('Listening on port', server.address().port);
   });
---
+++ users.js
@@ -0,0 +1,22 @@
+const userRoutes = (app, fs) => {
+	// variables
+	const dataPath = './data/users.json';
+  
+	//  defining an endpoint returning my name and 
+    // the url from which the query is made
+	app.get('/users', (req, res) => {
+	  fs.readFile(dataPath, 'utf8', (err, data) => {
+		if (err) {
+		  throw err;
+		}
+       res.send(JSON.parse(data));
+	  });
+	});
+  };
+
+  
+  module.exports = userRoutes;
+
+
+
+  
