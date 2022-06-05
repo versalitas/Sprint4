e50c235f4bd3539accd12679def1c107e1383b37
MK <caprichismo@gmail.com>
2022-06-05 19:13:04 +0200

    editing index file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-//importing dependencies
+
+//https://medium.com/@onejohi/building-a-simple-rest-api-with-nodejs-and-express-da6273ed7ca9
+//https://www.edureka.co/blog/rest-api-with-node-js/
+//https://expressjs.com/en/api.html
 //https://medium.com/@mmajdanski/express-body-parser-and-why-may-not-need-it-335803cd048c
+//https://stackoverflow.com/questions/59608660/rest-api-best-practise-for-routes
+
+
+//importing dependencies
 
 const express = require('express');
 
@@ -7,8 +14,6 @@ const express = require('express');
 const app = express();
 
 const fs = require('fs');
-
-const currentUrl = 'http://localhost';
 
 //PORT defining as environmental variable
 // in terminal $export PORT=3001 or another value 
@@ -18,8 +23,10 @@ const port = process.env.PORT || 3001;
 //handling routes
 const routes = require('./routes/routes.js')(app, fs);
 
-//add middleware for parsing json
+//add middleware for handling json
 app.use(express.json());
+//app.use(express.static(path.join(__dirname, './data')));
+//app.use(express.urlencoded({extended: false}));
 
 
 
