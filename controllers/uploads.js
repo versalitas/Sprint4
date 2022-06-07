dbaa408308405bab118b4dbe965a538b0b1c31e5
MK <caprichismo@gmail.com>
2022-06-07 13:00:29 +0200

    add validateExt

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,15 +1,32 @@
 //https://blog.logrocket.com/multer-nodejs-express-upload-file/
 //https://laratutorials.com/node-js-express-rest-api-image-upload-example/
+//https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 
-//http://localhost:3001/api/images
+
+
+const validateExt = (filename) =>
+{ const okExts = ['img','jpg','jpeg', 'png', 'svg', 'gif',];
+  const i = filename.lastIndexOf('.');
+  const ext = filename.substr(i);
+  return (i > -1 && okExts.includes(ext))? true: false;
+}
 
 
 const uploadImage = (res, req) => {
 try {  
+    
     //checks if 400 – Bad request
     if (!req.files) return res.status(400).send({ status: "fail", message: "File not found"}); 
-    //check extensions let ext = ['img','jpg','jpeg', 'png', 'svg', 'gif]
-    //
+    let isExtOk = validateExt(req.files);
+    if(isExtOk){
+
+    } else {
+      return res.status(415).send({ status: "fail", message: "Unsupported Media Type"}); 
+    }
+    );
+  }
+    
+    
 
 
 
@@ -21,7 +38,8 @@ try {
       }
 
 
-}
+
+
 
 
 module.exports = uploadImage;
--- routes/uploads.js
+++ routes/uploads.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const api = express.Router();
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const uploadImage = require('../controllers/uploads.js');
 
 
+
+const storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+      callback(null, '/data');
+    },
+    filename: function (req, file, callback) {
+      callback(null, file.fieldname);
+    }
+  });
+
+
 api.post('/uploads', upload.single(img), uploadImage);
+
+
+
+
+module.exports = api;
