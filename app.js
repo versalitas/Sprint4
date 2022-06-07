a72e79e85890101e4d552f3cd6d281fe266068ec
MK <caprichismo@gmail.com>
2022-06-07 09:44:17 +0200

    add install multer and cors changing names again sigh...

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict'
 
-var express = require("express");
+const express = require("express");
+const multer = require('multer');
+const cors = require('cors');
 
 //initialize
 const app = express();
@@ -11,16 +13,22 @@ const port = process.env.PORT || 3001;
 
 //import routes
 let api_routes = require('./routes/users.js');
+let uploads_routes = require('./routes/uploads.js')
 
+//enabling cors
+app.use(cors);
 //add middleware for handling json
 app.use(express.json());
 //app.use(express.static(path.join(__dirname, './data')));
 app.use(express.urlencoded({extended: false}));
 
 
+
+
 //base route
 
 app.use('/api', api_routes);
+app.use('/api/images', upload_routes);
 
 
 // starting the server
