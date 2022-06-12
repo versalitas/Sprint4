50a8969c1333be53e16ca1af8610afcabdbe7aa0
MK <caprichismo@gmail.com>
2022-06-12 23:12:28 +0200

    add first steps basic authentication

--- routes/time.js
+++ routes/time.js
@@ -2,17 +2,24 @@ const express = require('express');
 const api = express.Router();
 
 const getTime = require('../controllers/time.js');
+const cors = require('cors');
+const noCache = require('../middlewares/noCache.js');
 
 //adding middleware
-const cors = require('../middlewares/corsMiddleware.js');
-const noCache = require('../middlewares/noCache.js');
-//const checkAuth = require('../middlewares/checkAuth');
+
+api.use(cors());
+api.use(noCache);
+api.use
+
+
+
+
 
 
 //endpoint to get time
 api.post('/api',
-     //cors,
-    noCache,                         
+    //cors(),
+    //noCache,                         
     //checkAuth
   getTime);
 
