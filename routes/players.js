944e380b78dad32da4dbcfb55c96f4f1b99ff3af
MK <caprichismo@gmail.com>
2022-06-21 21:01:00 +0200

    something is amiss...

---
+++ routes/players.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const api = express.Router();
+
+//const {addPlayer, modifyName, showPlayer} = require('../controllers/players.js');
+const addPlayer = require('../controllers/players.js');
+
+
+
+
+//api.post('/api', addPlayer);
+api.get
+
+
+
+module.exports = api;
