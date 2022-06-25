51662f5968fba9eb5d6771f74ef179a7cdef531b
MK <caprichismo@gmail.com>
2022-06-25 20:35:46 +0200

    add deleteScores

--- routes/games.js
+++ routes/games.js
@@ -1,16 +1,12 @@
-
 const express = require('express');
 const api = express.Router();
-const {playGame, getScores} = require('../controllers/games.js')
 
+//controllers
+const {playGame, getScores, deleteScores} = require('../controllers/games.js')
 
-
+//routes
 api.post('/games/:id', playGame);
 api.get('/games/:id', getScores);
-
-/*
-DELETE /games/{id}: delete all rolls of player
-GET /games/{id}: list of all rounds of player (id)
-*/
+api.delete('/games/:id', deleteScores);
 
 module.exports = api;
