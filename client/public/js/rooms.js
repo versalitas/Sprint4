c4a05d5c87a52ccf97108ac4574d25aa23fbf799
MK <caprichismo@gmail.com>
2022-10-19 16:08:01 +0200

    responsive tests

--- client/public/js/rooms.js
+++ client/public/js/rooms.js
@@ -9,8 +9,7 @@ const createRoom = () => {
  }
 
 const joinRoom = (room) => {
-    // console.log('joinRoom', room)
-
+    
     // Check if room other than current
     if (sessionStorage.roomId === room.roomId) return;
 
@@ -34,30 +33,50 @@ const joinRoom = (room) => {
 }
 
 const displayRoom = (room) => {
-
+    //create new element for room list of buttons
     const btn = document.createElement('button');
-        if (room.roomName === 'Hall') {
+    
+    //check if room is default and join
+    if (room.roomName === 'Limbo') {
         btn.classList.add('room-btn-active');
         joinRoom(room);
-    }
-
+    }   
+    
+    //set values to text and id
     btn.textContent = room.roomName;
     btn.setAttribute('id', room.roomId);
     btn.classList.add('room-btn');
     btn.onclick = () => {
-
+        
+        //remove current active button
         if (sessionStorage.roomId) {
             document.getElementById(sessionStorage.roomId).classList.remove('room-btn-active')
         }
-
+        //update as active the chosen/new room 
         btn.classList.add('room-btn-active');
+        //and join...
         joinRoom(room);
-
     }
 
+    //snippet for handling responsive buttons
+    let roomItem = document.getElementById('room');
+    if (roomItem.classList.contains('responsive')) {
+
+        roomItem.classList.remove('responsive');
+        
+        //display user
+        let user = document.getElementById('user');
+        user.classList.remove('d-none');
+        
+        //display room
+        let chat = document.getElementById('chat');
+        chat.classList.remove('d-none');
+     }
+   
+    //retrieve roomlist and append the new room button
     const rooms = document.getElementById("roomList");
     rooms.append(btn);
-
+    //sort list alphabetically with default "Limbo" on top
     sortBtnList("roomList");
 }
 
@@ -65,3 +84,9 @@ const displayRoom = (room) => {
 const displayRoomUsers = (room, users) => {
     document.getElementById(room.roomId).textContent = `${room.roomName} (${users.length})`
 }
+
+
+
+
+
+        l
