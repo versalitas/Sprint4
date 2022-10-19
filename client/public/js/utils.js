58857e431f9db3f6cf983dc0409749b0ecfde475
MK <caprichismo@gmail.com>
2022-10-19 16:09:06 +0200

    trying to figure out responsive

--- client/public/js/utils.js
+++ client/public/js/utils.js
@@ -10,3 +10,51 @@ const sortBtnList = (btn) => {
         .forEach(li => buttonList.append(li));
 }
 
+
+//show users when @media min width
+function showUsers() {
+
+    let user = document.getElementById('user');
+    user.classList.toggle('responsive');
+
+    let room = document.getElementById('room');
+    room.classList.toggle('d-none');
+
+    let chat = document.getElementById('chat');
+    chat.classList.toggle('d-none');
+    
+    if (user.classList.contains('responsive')){
+        let userList = document.getElementById('userList');
+        userList.addEventListener('click', e => {
+            e.preventDefault();
+            user.classList.remove('responsive');
+            room.classList.remove('d-none');
+            chat.classList.remove('d-none');
+
+        });
+    }
+}
+
+function showRooms() {
+    
+    let room = document.getElementById('room');
+    room.classList.toggle('responsive');
+
+    let user = document.getElementById('user');
+    user.classList.toggle('d-none');
+
+    let chat = document.getElementById('chat');
+    chat.classList.toggle('d-none');
+
+    if (room.classList.contains('responsive')){
+        let roomList = document.getElementById('roomList');
+        roomList.addEventListener('click', e => {
+            e.preventDefault();
+            room.classList.remove('responsive');
+            user.classList.remove('d-none');
+            chat.classList.remove('d-none');
+
+        });
+    }
+}
+
